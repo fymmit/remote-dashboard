5eb5c8f89d5c6acc365732d172dfb59f12dd6a22
Timo Blomqvist <blomqvist.timo@gmail.com>
2020-12-12 07:12:34 +0200

    Add separate api route

--- api.js
+++ api.js
@@ -3,11 +3,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const loudness = require('loudness');
-const sendkeys = require('sendkeys');
-const { exec } = require('child_process');
 const { logger } = require('./src/middleware/loggingMiddleware.js');
 const { isValidUrl } = require('./src/utils/validation.js');
+const apiRouter = require('./routes/apiRouter.js');
 
 const { PORT } = process.env;
 
@@ -16,43 +14,13 @@ app.use(logger);
 app.use(cors());
 // app.use(express.static('src/ui'));
 app.use(express.static('front/build'));
+app.use('/api', apiRouter);
 
-app.get('/', (req, res) => {
+app.get('*', (_, res) => {
   // res.sendFile('index.html', { root: __dirname + '/src/ui' });
   res.sendFile('index.html', { root: __dirname + '/front/build' });
 });
 
-app.get('/volume', async (req, res) => {
-  const volume = await loudness.getVolume();
-  res.send(volume.toString());
-});
-
-app.post('/volume/:v', async (req, res) => {
-  const { v } = req.params;
-  const vol = Number(v);
-  if (vol <= 100 && vol >= 0) {
-    await loudness.setVolume(vol);
-  }
-  res.sendStatus(200);
-});
-
-app.post('/website', async (req, res) => {
-  const { url } = req.query;
-  // if (isValidUrl(url))
-  {
-    // exec(`start ${url}`);
-    await sendkeys(`^l${url}~`);
-    res.sendStatus(200);
-    return;
-  }
-  res.sendStatus(400);
-});
-
-app.post('/sendkeys', async (req, res) => {
-  const { keys } = req.body;
-  await sendkeys(keys);
-  res.sendStatus(200);
-});
 
 app.listen(PORT || 9000, () => {
   console.log('Listening on port', PORT);
---
+++ routes/apiRouter.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const loudness = require('loudness');
+const sendkeys = require('sendkeys');
+const { exec } = require('child_process');
+
+router.get('/volume', async (req, res) => {
+  const volume = await loudness.getVolume();
+  res.send(volume.toString());
+})
+router.post('/volume/:v', async (req, res) => {
+  const { v } = req.params;
+  const vol = Number(v);
+  if (vol <= 100 && vol >= 0) {
+    await loudness.setVolume(vol);
+  }
+  res.sendStatus(200);
+})
+router.post('/website', async (req, res) => {
+  const { url } = req.query;
+  // if (isValidUrl(url))
+  {
+    // exec(`start ${url}`);
+    await sendkeys(`^l${url}~`);
+    res.sendStatus(200);
+    return;
+  }
+  // res.sendStatus(400);
+})
+router.post('/sendkeys', async (req, res) => {
+  const { keys } = req.body;
+  await sendkeys(keys);
+  res.sendStatus(200);
+});
+
+module.exports = router;
